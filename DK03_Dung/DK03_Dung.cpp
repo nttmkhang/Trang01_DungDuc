5bf71654ed66a078c08cfb2a80a2f5227acb050b
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-11 22:47:45 +0700

    Dung

---
+++ DK03_Dung/DK03_Dung.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+string ChenhLech(int, int);
+int main()
+{
+	int a, b;
+	cin >> a;
+	cin >> b;
+	cout << ChenhLech(a, b);
+	return 0;
+}
+
+string ChenhLech(int x, int y)
+{
+	stringstream stream;
+	if (x > y)
+		stream << x - y;
+	else
+		stream << y - x;
+	return stream.str();
+}
