e9707edd6a2e8c0917659358af5963ce1a944695
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-11 11:02:45 +0700

    Dung

---
+++ CB04_Dung/CB04_Dung.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+	long a;
+	cin >> a;
+	long b;
+	cin >> b;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	if (b == 0)
+		cout << "ERROR";
+	else
+		cout << fixed << setprecision(2) << (float)a / b;
+	return 0;
+}
