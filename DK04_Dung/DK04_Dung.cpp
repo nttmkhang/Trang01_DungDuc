039055063ce3245e6c046e6b5ee2019a8b163485
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-11 23:00:01 +0700

    Dung

---
+++ DK04_Dung/DK04_Dung.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	double x;
+	cin >> x;
+	cout << round(x);
+	return 0;
+}
