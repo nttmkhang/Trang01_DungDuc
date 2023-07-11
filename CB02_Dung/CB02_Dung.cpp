421067363e4308105c380529a26b729625b21966
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-11 10:54:23 +0700

    Dung

---
+++ CB02_Dung/CB02_Dung.cpp
@@ -0,0 +1,23 @@
+﻿// CB02_Dung.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+	return 0;
+}
+
