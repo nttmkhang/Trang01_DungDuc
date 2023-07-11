5226d3075db2dabce50c0be085183f15aeb48055
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-11 10:49:43 +0700

    Dung

---
+++ CB01_dUNG/CB01_dUNG.cpp
@@ -0,0 +1,11 @@
+﻿// CB01_dUNG.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Lap Trinh Khong Kho!";
+	return 0;
+}
--- VL15_Dung/VL15_Dung.cpp
+++ VL15_Dung/VL15_Dung.cpp
@@ -24,7 +24,6 @@ int main()
 	int b;
 	cin >> b;
 
-
 	RutGon(a, b);
 	return 0;
 }
