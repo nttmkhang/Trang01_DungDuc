04e6102f5c6c3d4c537cc5a386c72a1e271b1399
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-11 13:43:20 +0700

    Dung

---
+++ CB07_Dung/CB07_Dung.cpp
@@ -0,0 +1,24 @@
+﻿// CB07_Dung.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	float r;
+	cin >> r;
+	cout << fixed << setprecision(3) << r * 2 * 3.14;
+	cout << " ";
+	cout << fixed << setprecision(3) << r * r * 3.14;
+	return 0;
+}
