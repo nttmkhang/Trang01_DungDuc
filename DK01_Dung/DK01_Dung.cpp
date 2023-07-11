38d60fb35ab11fad3520945c8d414cfcb9609eee
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-11 22:35:37 +0700

    Dung

---
+++ DK01_Dung/DK01_Dung.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+string LonNhat(int, int);
+
+int main()
+{
+	int a, b;
+	cin >> a;
+	cin >> b;
+	cout << LonNhat(a, b);
+	return 0;
+}
+
+string LonNhat(int x, int y)
+{
+	stringstream stream;
+	if (x > y)
+		stream << x;
+	else
+		stream << y;
+	return stream.str();
+}
