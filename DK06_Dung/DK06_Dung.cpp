2983e9fd02f0cad3e4f07c6ebe5d6ed802714390
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-13 10:45:12 +0700

    Dung

---
+++ DK06_Dung/DK06_Dung.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <cmath>
+using namespace std;
+
+string GiaiBacNhat(int, int);
+
+int main()
+{
+    int a;
+    cin >> a;
+    int b;
+    cin >> b;
+    cout << GiaiBacNhat(a, b);
+    return 0;
+}
+
+string GiaiBacNhat(int a, int b)
+{
+    string temp;
+    if (a == 0)
+        if (b == 0)
+            temp = "WOW";
+        else
+            temp = "NO";
+    else
+    {
+        float x = (float)(-b) / a;
+        stringstream stream;
+        stream << fixed << setprecision(2) << x;
+        temp = stream.str();
+    }
+    return temp;
+}
