1ea444696c46694e0e630ee3f10f39f471927868
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-13 13:33:38 +0700

    Dung

---
+++ DK08_Dung/DK08_Dung.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <cmath>
+using namespace std;
+
+string Tinh(double, char, double);
+
+int main()
+{
+	double a;
+	double b;
+	char m;
+	cin >> a;
+	cin >> m;
+	cin >> b;
+
+	cout << Tinh(a, m, b);
+	return 0;
+}
+
+string  Tinh(double a, char m, double b)
+{
+	stringstream stream;
+	if (m == '/' && b == 0)
+	{
+		stream << "Math Error";
+		return stream.str();
+	}
+	double kq;
+	switch (m)
+	{
+	case '+':
+		kq = (a + b);
+		break;
+	case '-':
+		kq = a - b;
+		break;
+	case '*':
+		kq = a * b;
+		break;
+	case '/':
+		kq = a / b;
+		break;
+	}
+	stream << fixed << setprecision(2) << kq;
+	return stream.str();
+}
