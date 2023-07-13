cd5a301acf19d2081b93ce869221c1f39ba8eec4
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-13 10:55:09 +0700

    Dung

---
+++ SUM1_Dung/SUM1_Dung.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string Tong(long);
+
+int main()
+{
+	long n;
+	cin >> n;
+	cout << Tong(n);
+	return 0;
+}
+
+string Tong(long x)
+{
+	stringstream stream;
+	if (x % 2 == 0)
+		stream << (x / 2) * (x + 1);
+	else
+		stream << ((x + 1) / 2) * x;
+	return stream.str();
+}
