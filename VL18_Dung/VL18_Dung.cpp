c13d98570639ce084b1b5d9876b148b3d261bb27
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-13 20:15:31 +0700

    Dung

---
+++ VL18_Dung/VL18_Dung.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string DaoNguoc(string);
+
+int main()
+{
+	string n;
+	cin >> n;
+
+	cout << DaoNguoc(n);
+	return 0;
+}
+
+string DaoNguoc(string x)
+{
+	stringstream stream;
+	string dn;
+	long cs = x.length();
+	while (x[cs - 1] == '0')
+		cs--;
+	for (int i = 0; i <= cs - 1; i++)
+		dn = x[i] + dn;
+	stream << dn;
+	return stream.str();
+}
