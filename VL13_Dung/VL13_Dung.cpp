bbb8348153220efdc9fbd7b0f948d03907ddc64c
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-13 20:41:02 +0700

    Dung

---
+++ VL13_Dung/VL13_Dung.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string process(long);
+bool KtHoanHao(long);
+
+int main()
+{
+	long n;
+	cin >> n;
+	cout << process(n);
+	return 0;
+}
+
+string process(long x)
+{
+	stringstream stream;
+	if (KtHoanHao(x))
+		stream << "YES";
+	else
+		stream << "NO";
+	return stream.str();
+}
+
+bool KtHoanHao(long x)
+{
+	long s = 0;
+	for (long i = 1; i < x; i++)
+		if (x % i == 0)
+			s += i;
+	if (s == x)
+		return true;
+	else
+		return false;
+}
