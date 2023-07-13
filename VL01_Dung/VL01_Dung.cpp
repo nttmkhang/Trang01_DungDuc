c607c436d5f229e53186c1ea582c7554502183b7
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-13 10:47:29 +0700

    Dung

---
+++ VL01_Dung/VL01_Dung.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void LietKe(long, long);
+
+int main()
+{
+	long a;
+	cin >> a;
+	long b;
+	cin >> b;
+	LietKe(a, b);
+	return 0;
+}
+
+void LietKe(long x, long y)
+{
+	for (long i = x; i <= y; i++)
+		cout << i << " ";
+}
