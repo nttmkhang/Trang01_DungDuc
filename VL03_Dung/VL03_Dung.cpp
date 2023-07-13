c666e42f82fd3ce547a1b35ab5d5f0d2040626e7
TruongHoangTanDugn <tandungtruonghoang@gmail.com>
2023-07-13 10:51:55 +0700

    Dung

---
+++ VL03_Dung/VL03_Dung.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+string Tong(int);
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << Tong(n);
+	return 0;
+}
+
+string Tong(int x)
+{
+	stringstream stream;
+	int s = 0;
+	for (int i = 2; i <= x; i++)
+		s += i;
+	stream << s + 2 * x;
+	return stream.str();
+}
